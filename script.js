d45062084757bbb04ebdbb8076c756c98a4f549a
vitoriaaraujosilva22-afk <vitoria.araujo.silva22@escola.pr.gov.br>
2026-09-09 17:25:56 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const botao = document.querySelector("#botaoMensagem");
+const mensagem = document.querySelector("#mensagemInterativa");
+botao.addEventListener("click", mostrarMensagem);
+function mostrarMensagem() {
+    mensagem.textContent =
+    "Voce acabou de criar uma interacao com JavaScript!";
+}
